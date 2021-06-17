0f8471e40e02179730e7f6e103b767230bea334f
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-06-17 19:58:03 -0400

    define columns of our table.

--- models/users.js
+++ models/users.js
@@ -3,5 +3,23 @@ const bcrypt = require("bcryptjs");
 
 //exporting this model to our index
 module.exports = function(sequelize, DataTypes) {
- 
+  var User = sequelize.define("Users", {
+    // define columns of our table
+    nickname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
 };
