23afc41d8420beeb6b78312c3ff8ca110ff541b2
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-06-17 19:41:28 -0400

    setting packages for app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+//packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./models/index.js");
+const routes = require("./routes");
+const passport =  require("./config/passport");
+const session = require("express-session");
